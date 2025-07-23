f0742553a7d322e90463e5f17d39632c48b215d3
Dherik Domingos <dherikdomingos@gmail.com>
2025-07-23 16:12:21 -0300

    urls e views

--- tacomachave/urls.py
+++ tacomachave/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from core.views import index
+from core.views import index, perfil
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index, name="index")
+    path('', index, name="index"),
+    path('/perfil', perfil, name="perfil")
 ]
