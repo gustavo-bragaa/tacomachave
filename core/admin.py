e9cc5d2854245834264b272ab2e89f5a7f5e9958
Dherik Domingos <dherikdomingos@gmail.com>
2025-11-06 15:19:50 -0300

    joi

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Chave, Usuario, Movimentacao, Permissao
 
-# Register your models here.
+# Registrando os modelos no site de administração do Django
+
+# Estes são os "CRUDs sem dependências" (Entrega 30/10)
+admin.site.register(Chave)
+admin.site.register(Usuario)
+
+# Estes são os "CRUDs com dependências" (Entrega 06/11)
+admin.site.register(Movimentacao)
+admin.site.register(Permissao)
